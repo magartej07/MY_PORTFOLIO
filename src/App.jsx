1e0d1d807ec2ce443b703331cb2ec50c20bb4aee
Tej Magar <your_email@example.com>
2025-02-24 13:36:54 +0000

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>MA TEJ HO</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count no count {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
